5545bd89ba104a350306126f62f5f809063c62b3
Antonio-Dielson <hammstark0@gmail.com>
2024-08-19 16:59:19 -0300

    Update criar_registro.py

--- src/criar_registro.py
+++ src/criar_registro.py
@@ -1,49 +1,45 @@
+from datetime import datetime
 from utilitarios.calcular_tempo import tempo
 from utilitarios.entrada_data import validar_data
-from utilitarios.validar_generic import ValidarDadosGeneric
+from utilitarios.validacao import validar_tipo, validar_valor
 
 
-def criar_registro(): #separar dia, mes e ano
-    """Cria um novo registro financeiro com interação do usuário."""
+def criar_registro() -> dict:
+    '''
+    Cria um novo registro financeiro com interação do usuário.
 
-    data = validar_data()
-    while True:
-        tipo = input("Digite o tipo de movimentação (Receita, Despesa ou Investimento): ").capitalize()
-        if tipo in ['Receita','Despesa','Investimento']:
-            break
-        else:
-            print('Erro, tipo invalido')
-    while True:
-        valor = input("Digite o valor: ")
-        try:
-            valor = float(valor)
-            break
-        except ValueError:
-            print('Digite apenas valores numericos')
+    Solicita ao usuário que digite uma data, tipo de movimentação (Receita, Despesa ou Investimento),
+    e o valor. 
+    Se o usuário selecionar 'Investimento' ela irá calcular o montante e o rendimento com base em um percentula de juros fixo.
+
+    Returns:
+        Dict
+        Retorna um dicionário representando o registro financeiro que contém as chaves:
+        'id', 'data', 'tipo', 'valor', 'montante', e 'rendimento'.
+    '''
+    data = validar_data('Insira uma data no formáto válido, dd/mm/yyyy')
+    tipo = validar_tipo('Digite o tipo que deseja criar. [Receita, Despesa, Investimento]: ')
+    valor = validar_valor()
 
     montante = None
     rendimento = None
 
     if tipo == 'Investimento':
-        while True:
-            juros = input('Digite o percentual mensal de juros do investimento:')
-            try:
-                juros = float(juros)
-                break
-            except ValueError:
-                print('Erro, digite apenas números')
 
-        montante_inicial = valor * (1+((juros)/100))**(tempo(data))
+        juros = 0.01
+
+        montante_inicial = valor * (1+((juros)))**(tempo(data['data_completa']))
         montante = round(montante_inicial, 2)
         rendimento_inicial = montante - valor
         rendimento = round(rendimento_inicial, 2)
 
     registro = {
-        'id': 0,
         'data': data,
         'tipo': tipo,
         'valor': valor if tipo != 'Despesa' else -valor, 
         'montante': montante,
-        'rendimento': rendimento}
+        'rendimento': rendimento,
+        'data_atualizacao': None
+        }
 
     return registro
