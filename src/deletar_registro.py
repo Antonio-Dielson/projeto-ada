638f80fb86bbd98e1dd796269b483965fccc8798
Antonio-Dielson <hammstark0@gmail.com>
2024-08-19 17:48:19 -0300

    Update deletar_registro.py

--- src/deletar_registro.py
+++ src/deletar_registro.py
@@ -1,6 +1,20 @@
-def deletar_registro(registros):
-    """Deleta um registro financeiro com interação do usuário."""
+from utilitarios.validacao import validar_indice
+def deletar_registro(registros: list[dict])-> None:
+    
+    '''Deleta um registro financeiro, se solicitado pelo usuário.
 
+            Essa função vai exibir uma lista de registros e permitir que o usuário delete o registro que escolher.
+            Se a lista estiver vazia uma mensagem será exibida informando que nenhum registro foi encontrado.
+            Se a informação fornecida pelo usuário estiver incorreta, a função também notificará.
+        
+        Args:
+            registros (list[dict]):
+                Recebe uma lista de dicionários, cada dicinário representa um registro.
+
+        Returns:
+            None: 
+                Não retorna nenhum valor, apenas deleta o registro da lista.
+    '''
     if not registros:
         print("Nenhum registro encontrado.")
         return
@@ -8,9 +22,6 @@ def deletar_registro(registros):
     for i, registro in enumerate(registros):
         print(f"{i}: {registro}")
 
-    indice = int(input("Índice do registro a ser deletado: "))
-
-    if 0 <= indice <= len(registros):
-        del registros[indice]
-    else:
-        print("Índice inválido.")
+    indice = validar_indice(registros)
+    del registros[indice]
+    print(f'{indice} deletado com sucesso')
